88a7f8d1de83a18b837b61343002c16358ae791c
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-05-13 23:05:47 +0200

    Added env file template and config for firebase

---
+++ client/src/firebase/firebaseconfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: 'the-cocktail-app.firebaseapp.com',
+  projectId: 'the-cocktail-app',
+  storageBucket: 'the-cocktail-app.appspot.com',
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
