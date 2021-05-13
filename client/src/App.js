7b949213ef0c9e50e76420fbd4a370cbd2a06c42
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-05-14 01:41:05 +0200

    Implemented redux action login and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
+import { checkUserSession, signOutUserStart } from './redux/user/user.actions';
 import Login from './containers/Login';
+import WithAuth from './hoc/WithAuth';
 
 function App() {
+  const dispatch = useDispatch();
+  const signOut = () => {
+    dispatch(signOutUserStart());
+  };
+
+  useEffect(() => {
+    dispatch(checkUserSession());
+  }, []);
+
   return (
-    <Router>
-      <div className='App'>
-        <Link to='/'>Home</Link>
-        <Link to='/login'>Login page</Link>
-      </div>
+    <div className='App'>
       <Switch>
-        <Route path='/login'>
-          <Login />
-        </Route>
-        {/* <Route path="/">
-            <Home />
-          </Route> */}
+        <Route
+          exact
+          path='/'
+          render={() => (
+            <>
+              <h1>Home page</h1>
+            </>
+          )}
+        />
+        <Route path='/login' render={() => <Login />} />
+        <Route
+          path='/auth'
+          render={() => (
+            <WithAuth>
+              <h1>Auth page</h1>
+              <span onClick={signOut}>Logout</span>
+            </WithAuth>
+          )}
+        />
       </Switch>
-    </Router>
+    </div>
   );
 }
 
