f7be799b2bfd5534c734ae2b993809c6f7475443
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-05-19 20:48:16 +0200

    Redux - implement SignUp and SignIn email

--- client/src/redux/user/user.sagas.js
+++ client/src/redux/user/user.sagas.js
@@ -26,19 +26,7 @@ export function* getSnapshotFromUserAuth(user, additionalData = {}) {
   }
 }
 
-export function* googleSignIn() {
-  try {
-    const { user } = yield auth.signInWithPopup(GoogleProvider);
-    yield getSnapshotFromUserAuth(user);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-export function* onGoogleSignInStart() {
-  yield takeLatest(userTypes.GOOGLE_SIGN_IN_START, googleSignIn);
-}
-
+// Check session
 export function* isUserAuthenticated() {
   try {
     const userAuth = yield getCurrentUser();
@@ -53,6 +41,22 @@ export function* onCheckUserSession() {
   yield takeLatest(userTypes.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
+// Google signin
+export function* googleSignIn() {
+  try {
+    const { user } = yield auth.signInWithPopup(GoogleProvider);
+    yield getSnapshotFromUserAuth(user);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function* onGoogleSignInStart() {
+  yield takeLatest(userTypes.GOOGLE_SIGN_IN_START, googleSignIn);
+}
+
+// Signout user
+
 export function* signOutUser() {
   try {
     yield auth.signOut();
@@ -66,10 +70,48 @@ export function* onSignOutUserStart() {
   yield takeLatest(userTypes.SIGN_OUT_USER_START, signOutUser);
 }
 
+// Singup
+
+export function* signUpUser({ payload: { displayName, email, password } }) {
+  yield put(userError(''));
+  try {
+    const { user } = yield auth.createUserWithEmailAndPassword(email, password);
+    const additionalData = { displayName };
+    yield getSnapshotFromUserAuth(user, additionalData);
+  } catch (err) {
+    yield put(userError(err.message));
+  }
+}
+
+export function* onSignUpUserStart() {
+  yield takeLatest(userTypes.SIGN_UP_USER_START, signUpUser);
+}
+
+// Email signin
+
+export function* emailSignIn({ payload: { email, password } }) {
+  try {
+    const { user } = yield auth.signInWithEmailAndPassword(email, password);
+    yield getSnapshotFromUserAuth(user);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export function* onEmailSignInStart() {
+  yield takeLatest(userTypes.EMAIL_SIGN_IN_START, emailSignIn);
+}
+
+// Password reset
+
+// Sagas
+
 export default function* userSagas() {
   yield all([
     call(onCheckUserSession),
     call(onSignOutUserStart),
     call(onGoogleSignInStart),
+    call(onEmailSignInStart),
+    call(onSignUpUserStart),
   ]);
 }
