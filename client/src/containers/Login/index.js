f7910f82752c27f973bf86e6295563f7a7665147
Robert Jaskólski <61465546+RobertJaskolski@users.noreply.github.com>
2021-05-19 13:32:58 +0200

    Edit Login container

--- client/src/containers/Login/index.js
+++ client/src/containers/Login/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import './styles.scss';
 import Button from '../../components/form/Button';
+import Input from '../../components/form/Input';
 import { googleSignInStart } from '../../redux/user/user.actions';
 import { Link, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import GoogleLogo from '../../assets/googleLogo.png';
+import FacebookLogo from '../../assets/facebookLogo.png';
 
 const mapState = ({ user }) => ({
   currentUser: user['currentUser'],
@@ -25,10 +28,23 @@ function Login() {
   }, [currentUser]);
 
   return (
-    <div>
-      <h1>Login component</h1>
-      <Button onClick={handleGoogleSignIn}>Zaloguj przez google</Button>
-    </div>
+    <section className='login'>
+      <div>
+        <h1>Login</h1>
+        <Input placeholder='Email' type='text' />
+        <Input placeholder='Password' type='password' />
+        <Button>Signin</Button>
+        <Button onClick={handleGoogleSignIn}>
+          <img src={GoogleLogo} alt='google logo' />
+          Signin with Google
+        </Button>
+        <Button>
+          <img src={FacebookLogo} alt='google logo' />
+          Signin with Facebook
+        </Button>
+        <p>Forgot password?</p>
+      </div>
+    </section>
   );
 }
 
