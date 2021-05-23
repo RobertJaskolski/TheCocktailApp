fa5a402857d1f3d677fe97db0d8fe2383df616e5
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-05-24 00:50:18 +0200

    created random drinks page

--- client/src/containers/Random/index.js
+++ client/src/containers/Random/index.js
@@ -1,16 +1,46 @@
-const Random = () => {
-  return (
-    <div>
-      <h1>Random drinks</h1>
-      <h2>Choose one from three below.</h2>
+import { useCallback, useEffect, useState } from "react";
+import RandomDrinkCard from "../../components/Random/RandomDrinkCard";
+import { readRandomDrinks } from "../../api/drinksClient";
+import "./styles.scss";
 
-      <div>
-        <div>Drink one..</div>
-        <div>Drink two..</div>
-        <div>Drink three..</div>
+const Random = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [drinks, setDrinks] = useState([]);
+
+  const loadRandomDrinks = useCallback(() => {
+    if (drinks.length === 0) {
+      readRandomDrinks().then((data) => {
+        console.log(data);
+        setDrinks(data.drinks);
+        setIsLoading(false);
+      });
+    }
+  }, [drinks]);
+
+  useEffect(() => {
+    loadRandomDrinks();
+  }, [loadRandomDrinks]);
+
+  if (isLoading || drinks.length === 0) {
+    return <div>Loading..</div>;
+  } else {
+    return (
+      <div className="random-drinks__container">
+        <h1 className="random-drinks__title">Random drinks</h1>
+        <h2 className="random-drinks__subtitle">
+          Choose one of the three drinks below.
+        </h2>
+
+        <div className="random-drinks__cards-wrapper">
+          <RandomDrinkCard drink={drinks[0]} />
+          <RandomDrinkCard drink={drinks[1]} />
+          <RandomDrinkCard drink={drinks[2]} />
+        </div>
+
+        <button onClick={() => setDrinks([])}>Let's draw again!</button>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Random;
