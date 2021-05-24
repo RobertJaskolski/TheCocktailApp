ac15857e7f879338334e4bcd039430e95a735952
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-05-25 00:27:01 +0200

    Test loading by ingredients

--- client/src/containers/IngredientSearch/index.js
+++ client/src/containers/IngredientSearch/index.js
@@ -1,24 +1,57 @@
 import "./styles.scss";
-import { useCallback, useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 import IngredientRowItem from "../../components/IngredientSearch/IngredientRowItem";
+import {readDrinksListByIngredients} from "../../api/ingredientsRepository";
 
 
 export default function IngredientSearch() {
     const [isLoading, setIsLoading] = useState(true);
-    const ingredients = [{ name: "Vodka", selected: false }, { name: "Lemon", selected: false }, { name: "Water", selected: false }]
+    const [drinks, setDrinks] = useState([]);
+
+    const loadDrinks = useCallback(() => {
+        if (drinks.length === 0) {
+            let drinksList = 'Dry_Vermouth,Gin,Anis'
+            readDrinksListByIngredients(drinksList).then((data) => {
+                console.log(data);
+                setIsLoading(false);
+            });
+        }
+    }, [drinks]);
+
+    useEffect(() => {
+        loadDrinks();
+    }, [loadDrinks]);
+
+    const ingredients = [
+        {
+            name: "Vodka",
+            selected: false
+        },
+        {
+            name: "Lemon",
+            selected: false
+        },
+        {
+            name: "Water",
+            selected: false
+        }
+    ]
+
+
     return (
         <div className="ingredients-search__container">
             <h1 className="ingredients-search__title">Search by ingredient</h1>
             <h2 className="ingredients-search__subtitle">
                 Select ingredients
-        </h2>
+            </h2>
 
             <div className="ingredients-search__cards-wrapper">
-                {ingredients.map(x => <IngredientRowItem ingredient={x} />)}
-
+                {ingredients.map(x => <IngredientRowItem ingredient={x}/>)}
             </div>
 
-
+            <button onClick={() => setDrinks([])}>
+                Find!
+            </button>
         </div>
     )
 }
