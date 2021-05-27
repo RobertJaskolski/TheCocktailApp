94619c1d212aa3e886e3e3b18b1fdeed9bd4501d
Piotr Sobieszczyk <51410953+Sobiechh@users.noreply.github.com>
2021-05-27 22:04:03 +0200

    Code clean Ingredient Search index container

--- client/src/containers/IngredientSearch/index.js
+++ client/src/containers/IngredientSearch/index.js
@@ -1,19 +1,23 @@
 import "./styles.scss";
-import { useCallback, useEffect, useState } from "react";
+import {useCallback, useEffect, useState} from "react";
 import IngredientRowItem from "../../components/IngredientSearch/IngredientRowItem";
-import { fetchDrinksListByIngredients } from "../../api/ingredientsRepository";
+import fetchDrinksListByIngredients from "../../api/ingredientsRepository";
 
 
 export default function IngredientSearch() {
     const [isLoading, setIsLoading] = useState(true);
     const [drinks, setDrinks] = useState([]);
-    const ingredientsSample = [{ id: "002", name: "Vodka", selected: false }, { id: "001", name: "Lemon", selected: false }, { id: "003", name: "Water", selected: false }]
+    const ingredientsSample = [{id: "002", name: "Vodka", selected: false}, {
+        id: "001",
+        name: "Lemon",
+        selected: false
+    }, {id: "003", name: "Water", selected: false}]
     const [ingredients, setIngredients] = useState(ingredientsSample)
 
     const loadDrinks = useCallback(() => {
         if (drinks.length === 0) {
             let drinksList = 'Dry_Vermouth,Gin,Anis'
-            readDrinksListByIngredients(drinksList).then((data) => {
+            fetchDrinksListByIngredients(drinksList).then((data) => {
                 console.log(data);
                 setIsLoading(false);
             });
@@ -25,15 +29,13 @@ export default function IngredientSearch() {
     }, [loadDrinks]);
 
 
-
     const handleIngredientSelection = id => {
         console.log(id)
         setIngredients(ingredients.map(
             item => {
                 if (item.id === id) {
-                    return { ...item, selected: !item.selected };
-                }
-                else {
+                    return {...item, selected: !item.selected};
+                } else {
                     return item;
                 }
             }
@@ -48,7 +50,7 @@ export default function IngredientSearch() {
             </h2>
 
             <div className="ingredients-search__list_container">
-                {ingredients.map(x => <IngredientRowItem ingredient={x} onSelect={handleIngredientSelection} />)}
+                {ingredients.map(x => <IngredientRowItem ingredient={x} onSelect={handleIngredientSelection}/>)}
             </div>
 
             <button onClick={() => setDrinks([])}>
